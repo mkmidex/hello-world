8e718c83d23aac58cd679121c81e20d17dce95c7
mkmidex <afolabimichaelkayode02@gmail.com>
2025-09-06 12:12:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let ismarried = true;
+
+console.log( ismarried, typeof ismarried);
+
+let d = "alabi";
+
+console.log(d)
+
+const g = 44;
+console.log(g);
+
+var m = 55;
+var m =33;
+
+console.log(m);
+
+
+let q = 3;
+let r = 4;
+console.log(q+r);
+console.log(4%3);
+console.log(q/r);
+console.log(q**r);
+console.log(q-r);
+console.log(q!=r);
+console.log(q==r);
+console.log(q===r);
